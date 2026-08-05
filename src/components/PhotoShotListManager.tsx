9093d30bec5f6b558bc04b73a8bda61543d4d3d9
Jordan Lee <jordanleeproductions@gmail.com>
2026-08-04 22:07:05 -0400

    fix(photos): import Vendor type in PhotoShotListManager.tsx for PHOTO-2

--- src/components/PhotoShotListManager.tsx
+++ src/components/PhotoShotListManager.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { PhotoShot } from '@/lib/sheets/types';
+import { PhotoShot, Vendor } from '@/lib/sheets/types';
 import { 
   Camera, 
   Plus, 
